c803c0f1c718c6a2e2d43f74ee4f5b1bbed81451
Pavlov Nikita <nikitapavlov2015@gmail.com>
2024-02-13 23:40:20 +0300

    fix Makefile README .eslintrc

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    const name = readlineSync.question('May I have your name? ');
-    const greetingSentence = `Hello, ${name}!`;
-    console.log(greetingSentence);
+  const name = readlineSync.question('May I have your name? ');
+  const greetingSentence = `Hello, ${name}!`;
+  console.log(greetingSentence);
 };
 export default greeting;
